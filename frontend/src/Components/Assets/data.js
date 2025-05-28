52b45c088bbf12dbf3c8632e293188cf314fd4ed
csprajwalpb <prajwalpb8055@gmail.com>
2025-05-28 10:45:32 +0530

    first-commit

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,41 @@
+import p17_img from './product_17.png'
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p27_img from './product_27.png'
+
+let data_product = [
+  {
+     id: 17,
+        name: "Custom Slim Fit Soft Cotton Polo Shirt",
+        category: "men",
+        image: p17_img,
+        new_price: 85.0,
+        old_price: 120.5,
+  },
+  {
+      id: 1,
+      name: "Women Ribbed Turtle Neck Cotton Pullover",
+      category: "women",
+      image: p1_img,
+      new_price: 50.0,
+      old_price: 80.5,
+  },
+  {
+      id: 2,
+      name: "Purple Ribbed V-Neck Puff Sleeves Acrylic Pullover",
+      category: "women",
+      image: p2_img,
+      new_price: 85.0,
+      old_price: 120.5,
+  },
+  {
+   id: 27,
+    name: "Girls White Ethnic Wear",
+    category: "kid",
+    image: p27_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+];
+
+export default data_product;
